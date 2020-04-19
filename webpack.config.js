d218daa2cf7caad878daecea1d26a4a612d39205
李斌 <libin@baikemy.com>
2020-04-19 17:05:31 +0800

    发布

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
-  // entry: './src/lib/index.js',
+  // entry: './src/main.js',
+  entry: './src/lib/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'diff.js',
-    library: 'diff',
+    library: 'liDiff',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
